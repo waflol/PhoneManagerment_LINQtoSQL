b0ac743b12c6154539027019ab2f25b6c553b0d0
waflol <lenguyenhunganh2001@gmail.com>
2021-06-20 22:50:15 +0700

    HA

--- BS Layer/BLTransactions.cs
+++ BS Layer/BLTransactions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Linq;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,53 +10,33 @@ namespace PhoneManagerment_LINQtoSQL.BS_Layer
 {
     class BLTransactions
     {
-        DBMain db = null;
-        string err;
-        public BLTransaction()
+        
+        public Table<Transaction> GetTransactions()
         {
-            db = new DBMain();
-        }
-        public DataSet GetPhones()
-        {
-            return db.ExcuteQueryDataSet("select * from Transactions", CommandType.Text);
+            DataSet ds = new DataSet();
+            QuanLyBanHangDataContext qlBH = new QuanLyBanHangDataContext();
+            return qlBH.Transactions;
         }
 
         public int returnMaxID()
         {
-            string sqlString = "select max(ID_transaction) as max_id from Transactions";
-            DataSet ds = db.ExcuteQueryDataSet(sqlString, CommandType.Text);
-            try
-            {
-                if (ds == null || ds.Tables.Count == 0 || ds.Tables[0].Rows.Count == 0)
-                    return 0;
-                else
-                    return int.Parse(ds.Tables[0].Rows[0]["max_id"].ToString());
-            }
-            catch (Exception error)
-            {
-                return 0;
-            }
-
+            
         }
 
         // them phone
         public bool addTransaction(int idtrans, int totalprice, string date, int idcus, string username)
         {
-            //string sqlString = "Insert Into Transactions Values("+idtrans.ToString()+","+totalprice.ToString()+",'"+date+"',"+idcus.ToString()+",'"+username+"')";
-            string sqlString = "Insert into Transactions Values(" + idtrans.ToString() + "," + totalprice.ToString() + ",CONVERT(datetime,'" + date + "',103)," + idcus.ToString() + ",'" + username + "')";
-            return db.MyExecuteNonQuery(sqlString, CommandType.Text, ref err);
+            
         }
         //xoa phone
         public bool deleteTransaction(ref string err, string IDtrans)
         {
-            string sqlString = "Delete From Transactions where ID_transaction = " + IDtrans.ToString();
-            return db.MyExecuteNonQuery(sqlString, CommandType.Text, ref err);
+            
         }
         // cap nhat phone
         public bool updateTransaction(int idtrans, int totalprice, DateTime date, int idcus, string username, ref string err)
         {
-            string sqlString = "Update Transactions Set Total_price = " + totalprice.ToString() + ", Date = " + date.ToString() + ",ID_customer = " + idcus.ToString() + ",username = '" + username + "' where ID_transaction = " + idtrans.ToString();
-            return db.MyExecuteNonQuery(sqlString, CommandType.Text, ref err);
+            
         }
     }
 }
