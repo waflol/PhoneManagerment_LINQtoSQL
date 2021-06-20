1ae4a04c26c12788d129eb943fc46d9a0bae96ed
waflol <lenguyenhunganh2001@gmail.com>
2021-06-20 13:00:26 +0700

    HA

---
+++ BS Layer/BLPhones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Data;
+
+namespace PhoneManagerment_LINQtoSQL.BS_Layer
+{
+    class BLPhones
+    {
+        public Table<Phone> GetPhones()
+        {
+            DataSet ds = new DataSet();
+            QuanLyBanHangDataContext qlBH = new QuanLyBanHangDataContext();
+            return qlBH.Phones;
+        }
+
+        public int ReturnMaxID()
+        {
+            QuanLyBanHangDataContext qlBH = new QuanLyBanHangDataContext();
+            Phone 
+        }
+
+    }
+}
---
+++ BS Layer/BLTransaction_Details.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneManagerment_LINQtoSQL.BS_Layer
+{
+    class BLTransaction_Details
+    {
+    }
+}
