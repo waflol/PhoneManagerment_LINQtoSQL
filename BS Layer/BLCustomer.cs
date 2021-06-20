cbf197276240b660f8c6808ecefd52c9a317f42c
waflol <lenguyenhunganh2001@gmail.com>
2021-06-20 23:16:49 +0700

    HA

--- BS Layer/BLCustomer.cs
+++ BS Layer/BLCustomer.cs
@@ -37,17 +37,51 @@ namespace PhoneManagerment_LINQtoSQL.BS_Layer
         // them phone
         public bool addCustomer(string cusname, string phonenumber, string address, int ID_customer)
         {
-            
+            QuanLyBanHangDataContext qlBH = new QuanLyBanHangDataContext()
+;           Customer cus = new Customer();
+            cus.ID_customer = ID_customer;
+            cus.Customer_Name = cusname;
+            cus.PhoneNumber = phonenumber;
+            cus.Address = address;
+
+            qlBH.Customers.InsertOnSubmit(cus);
+            qlBH.Customers.Context.SubmitChanges();
+            return true;
+
+
+
         }
         //xoa phone
         public bool deleteCustomer(ref string err, int ID_customer)
         {
-           
+            QuanLyBanHangDataContext qlBH = new QuanLyBanHangDataContext();
+
+            var Query = from item in qlBH.Customers
+                        where item.ID_customer == ID_customer
+                        select item;
+
+            qlBH.Customers.DeleteAllOnSubmit(Query);
+            qlBH.SubmitChanges();
+            return true;
         }
         // cap nhat phone
         public bool updateCustomer(string cusname, string phonenumber, string address, string idcus, ref string err)
         {
-            
+            QuanLyBanHangDataContext qlBH = new QuanLyBanHangDataContext();
+            var Query = (from item in qlBH.Customers
+                         where item.ID_customer == int.Parse(idcus)
+                         select item).SingleOrDefault();//Biến Query là biến tham chiếu lưu phone của 1 phone trong danh sách qlBH.Phones
+
+            if (Query != null)
+            {
+                Query.ID_customer = int.Parse(idcus);
+                Query.Customer_Name = cusname;
+                Query.PhoneNumber = phonenumber;
+                Query.Address = address;
+                qlBH.SubmitChanges();
+            }
+
+            return true;
         }
     }
 }
